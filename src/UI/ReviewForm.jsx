37764ebe0028bff990de3c9b7ce7697a2682dc4b
Conenn <sda1364@wgu.edu>
2022-07-06 11:26:58 -0500

    fixed api url

--- src/UI/ReviewForm.jsx
+++ src/UI/ReviewForm.jsx
@@ -89,7 +89,7 @@ function ReviewForm(props) {
       comment: comment,
     };
 
-    fetch("https://wgu-course-review-api.herokuapp.com/post/reviews", {
+    fetch("https://wgu-course-review-api.herokuapp.com/api/post/reviews", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
